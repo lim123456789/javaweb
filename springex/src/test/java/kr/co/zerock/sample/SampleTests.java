eceacfd4f4ba42df22b97b9b8532b6df89aacfa0
lim123456789 <ian320@naver.com>
2024-09-20 19:09:01 +0900

    4.1-4
    DataSource

--- springex/src/test/java/kr/co/zerock/sample/SampleTests.java
+++ springex/src/test/java/kr/co/zerock/sample/SampleTests.java
@@ -1,5 +1,10 @@
 package kr.co.zerock.sample;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,9 +21,21 @@ public class SampleTests {
 	@Autowired
 	private SampleService sampleService;
 	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Test
 	public void testService1() {
 		log.info(sampleService);
 		Assertions.assertNotNull(sampleService);
+	}
+	
+	@Test
+	public void testConnection() throws Exception {
+		Connection connection = dataSource.getConnection();
+		log.info(connection);
+		Assertions.assertNotNull(connection);
+		
+		connection.close();
 	}
 }
