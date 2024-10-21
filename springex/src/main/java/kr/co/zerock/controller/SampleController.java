d6b7dac0a9fc9d7bad9f55c2486442efabf41f35
lim123456789 <ian320@naver.com>
2024-10-21 16:09:41 +0900

    4.3-3
    exception handling

--- springex/src/main/java/kr/co/zerock/controller/SampleController.java
+++ springex/src/main/java/kr/co/zerock/controller/SampleController.java
@@ -41,4 +41,10 @@ public class SampleController {
 	public void ex6() {
 		
 	}
+	
+	@GetMapping("/ex7")
+	public void ex7(String p1, int p2) {
+		log.info("p1.........."+p1);
+		log.info("p2.........."+p2);
+	}
 }
